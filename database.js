0d90aa6bdf65b6eaf847dec0142c24e93941685e
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-06-12 16:20:01 -0600

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const user = process.env.USER_BD;
 const password = encodeURIComponent(process.env.PASSWORD_BD);
-const dbName = process.env.NAME_BD; 
+const dbName = process.env.NAME_BD;
+const ipDirection = process.env.IP_BD;
+const dbPort = process.env.PORT_BD;
 
 mongoose.connect(`mongodb://${user}:${password}@${ipDirection}:${dbPort}/${dbName}`)
     .then(db => console.log('Db is connected'))
